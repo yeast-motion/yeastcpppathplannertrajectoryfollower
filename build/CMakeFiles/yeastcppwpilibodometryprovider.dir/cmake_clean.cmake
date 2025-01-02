5bb7cd7532bbc9dd2bb5023045e1d4c1b2dee2d6
Michael Todd <eltodd@gmail.com>
2025-01-02 17:41:40 -0500

    Copy the cpp wpilib odometry project as a base

---
+++ build/CMakeFiles/yeastcppwpilibodometryprovider.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yeastcppwpilibodometryprovider.dir/CMakeFiles/3.28.3/CompilerIdC/CMakeCCompilerId.c.o"
+  "CMakeFiles/yeastcppwpilibodometryprovider.dir/CMakeFiles/3.28.3/CompilerIdC/CMakeCCompilerId.c.o.d"
+  "CMakeFiles/yeastcppwpilibodometryprovider.dir/CMakeFiles/3.28.3/CompilerIdCXX/CMakeCXXCompilerId.cpp.o"
+  "CMakeFiles/yeastcppwpilibodometryprovider.dir/CMakeFiles/3.28.3/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d"
+  "CMakeFiles/yeastcppwpilibodometryprovider.dir/src/wpilibodometryprovider.cpp.o"
+  "CMakeFiles/yeastcppwpilibodometryprovider.dir/src/wpilibodometryprovider.cpp.o.d"
+  "libyeastcppwpilibodometryprovider.a"
+  "libyeastcppwpilibodometryprovider.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/yeastcppwpilibodometryprovider.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/yeastcppwpilibodometryprovider.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for yeastcppwpilibodometryprovider.
+# This may be replaced when dependencies are built.
