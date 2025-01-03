0aebbca1a07e69194f5337198ff813dcbd92cd60
Michael Todd <eltodd@gmail.com>
2025-01-02 19:26:56 -0500

    Shift to tarject follower object

--- include/yeastcpppathplannertrajectoryfollower/pathplannertrajectoryfollower.hpp
+++ include/yeastcpppathplannertrajectoryfollower/pathplannertrajectoryfollower.hpp
@@ -3,25 +3,20 @@
 #include <iostream>
 #include <vector>
 
-#include "yeastcpp/data_structures/swerve_module_config.hpp"
 
-#include "yeastcpp/components/odometry_provider.hpp"
+#include "yeastcpp/components/follower.hpp"
 
-#include <frc/kinematics/SwerveDriveKinematics.h>
-#include <frc/kinematics/SwerveDriveOdometry.h>
 
 namespace yeast_motion
 {
-    class WPILibOdometryProvider : public OdometryProvider
+    class PathPlannerTrajectoryFollower : public Follower
     {
         public:
-            OdometrySample update (std::vector<SwerveModuleStatus> status, Rotation2D gyro_angle) override;
-            OdometrySample reset (OdometrySample reset_sample) override;
-            WPILibOdometryProvider(nlohmann::json characterization);
+            void begin(Trajectory trajectory) = 0;
+
+            MotionCommand follow(MotionState motion_state) = 0;
+            FollowerStatus status() = 0;
 
         private:
-            std::vector<SwerveModuleConfig> module_configs;
-            std::unique_ptr<frc::SwerveDriveKinematics<4>> kinematics;
-            std::unique_ptr<frc::SwerveDriveOdometry<4>> odometry;
     };
 }
